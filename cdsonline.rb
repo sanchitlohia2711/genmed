99df469f34ef9edd8cc5d162dfe44914f75f0f64
Archit Gupta <aghunk1@gmail.com>
2019-10-12 09:01:40 +0530

    bug fix

--- cdsonline.rb
+++ cdsonline.rb
@@ -18,11 +18,9 @@ class MyScraper
 
   def crawl
     puts "Started Crawling"
-    # page = AGENT.get(PATH1)
-    # start_crawling(page, 0, 2)
 
-    page = AGENT.get(PATH2)
-    start_crawling("", page, 0, 2)
+    page = AGENT.get(PATH3)
+    start_crawling("", page, 0, 0)
   end
 
   def start_crawling(anchor_text, page, initial_depth, final_depth)
@@ -37,23 +35,23 @@ class MyScraper
     end
   end
 
-  # parser for PATH1 and PATH2
+  # parser for PATH3
   def parse_table(page, anchor_text)
     begin
       ""
-      FILE.puts anchor_text.gsub(/[[:space:]]+/, ' ').strip
-      p anchor_text.gsub(/[[:space:]]+/, ' ').strip
-      tds = page.xpath("//html/body/table/tr[4]/td/table/tr").search("td")
-      tds = tds[(tds.count % 5)..-1]
+      tds = page.search("td")[2..-1]
       tds.each_slice(5) do |s|
-        row = s.map{|w| w.text.gsub(/[[:space:]]+/, ' ').strip}.join(",")
+        row = s.map{|w| w.text.gsub(/[[:space:]]+/, ' ').strip}.join("; ")
         p row
-        FILE.puts row
+        FILE2.puts row
       end
     rescue Exception => e
+      byebug
       p "-------------------------> e"
     end
   end
+
+
 
   # links {explore xpath: '/html/body/table/tbody/tr[3]/td[2]' {|e| puts e}}
 end
